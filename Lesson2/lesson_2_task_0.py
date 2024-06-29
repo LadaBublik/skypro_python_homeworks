e0d7a3e3b1e3651d6d1d1dd9cf628210576846e7
LadaBublik <ladabublik98@gmail.com>
2024-06-29 23:41:14 +0500

    HW2

---
+++ Lesson2/lesson_2_task_0.py
@@ -0,0 +1,4 @@
+my_age = input("Укажите Ваш возраст")
+age = int(my_age) + 1
+
+print("Ваш возраст: ", age)
---
+++ Lesson2/lesson_2_task_3.py
@@ -0,0 +1,7 @@
+def square(a):
+    result = a*a
+    return result
+
+
+s = square(4)
+print(s)
